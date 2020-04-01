6acf6c1981182008f5caff272804167f81a6b4c3
tehfedaykin <jennifer.wadella@gmail.com>
2020-03-31 23:13:22 -0500

    Auto-generated commit

--- src/app/common/prettyprint.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PrettyprintPipe } from './prettyprint.pipe';
-
-describe('PrettyprintPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PrettyprintPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
