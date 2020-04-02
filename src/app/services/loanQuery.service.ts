88a033c858356268e09b41b0b0e721d05450bcc8
tehfedaykin <jennifer.wadella@gmail.com>
2020-04-02 12:08:14 -0500

    adding switch code display to loans

--- src/app/services/loanQuery.service.ts
+++ src/app/services/loanQuery.service.ts
@@ -22,6 +22,7 @@ export class OutstandingLoanQuery extends Query<OutstandingLoanResponse> {
         remainingBalance
         roofColor
         streetNumber
+        friendCode
       }
     }
   `;
